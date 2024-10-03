07e81d166845c5828589c555d98e56a6d0641b1d
Raphael Kaviak Machnicki <raphael@email.com>
2024-10-03 14:13:20 -0300

    Read and Parse Modifiers and Values Options

--- ppk_parser.c
+++ ppk_parser.c
@@ -5,31 +5,41 @@
 #include <string.h>
 #include "ppk_parser.h"
 
-static int read_line (int fd, char * buf, int buf_size) {
-        if (!buf_size)
-                return 0;
+static inline void PPK_ERR(int err, const char* func_name){
+        printf("ERROR: %s: %s\n", strerror(err), func_name);
+        exit(-err);
+}
 
+static int read_line (int fd, char * buf, int buf_size) {
         int cur_pos = 0;
-        while (read (fd, buf + cur_pos, 1) > 0){
-                if (buf[cur_pos] == '\n')
+        ssize_t read_bytes = 0;
+        while (read_bytes = read (fd, buf + cur_pos, 1) > 0){
+                if (buf[cur_pos] == '\n'){
                         break;
+                }
                 cur_pos++;
 
                 if (cur_pos == buf_size)
-                        return -1;
+                        PPK_ERR (EIO,__func__);
         }
+ 
+        if(!read_bytes)
+                return 0;
 
         return cur_pos + 1;
 }
 
-static int * ppk_parse_ports(char* buf, int* size){
-        char port[5] = {0};
+static int* ppk_parse_ports(char* buf, int* size){
+        char port[PPK_STR_INT_SIZE] = {0};
         int index = 0, p_index = 0;
         int * ret = malloc(sizeof(int) * PPK_LINE_SIZE);
+        if (!ret)
+                PPK_ERR (ENOMEM,__func__);
+
         for(int i = 0; i < *size; i++){
                 if(buf[i] == ' ' || buf[i] == '\n'){
                         ret[index++] = atoi(port);   
-                        memset(port, 0, 5);
+                        memset(port, 0, PPK_STR_INT_SIZE);
                         p_index = 0;
                         continue;
                 }
@@ -37,189 +47,226 @@ static int * ppk_parse_ports(char* buf, int* size){
         }
         *size = index;
         ret = realloc(ret, index);
+        if (!ret)
+                PPK_ERR (ENOMEM,__func__);
+
         return ret;
 }
 
 static struct ppk_port_pair* ppk_read_ports(int fd){
         struct ppk_port_pair* port_pair = malloc(sizeof(struct ppk_port_pair));
+        if (!port_pair)
+                PPK_ERR (ENOMEM,__func__);
+
         char* buf = malloc(PPK_LINE_SIZE);
+        if (!buf)
+                PPK_ERR (ENOMEM,__func__);
+
         int size = read_line(fd, buf, PPK_LINE_SIZE);
-        if(size < 0){
-                goto out_error;
+        if(!size){
+                free(port_pair);
+                free (buf);
+                return 0;
         }
         port_pair->src_port = ppk_parse_ports(buf, &size);
+
+
         port_pair->size_src_port = size;
         size = read_line(fd, buf, PPK_LINE_SIZE);
-        if(size < 0){
-                goto out_error;
-        }
+
         port_pair->dst_port = ppk_parse_ports(buf, &size);
         port_pair->size_dst_port = size;
 
         free(buf);
         return port_pair;
+}
 
 
-out_error:
-        free(port_pair);
-        free(buf);
+static void ppk_read_int(int fd, int* field){
+        char buf[PPK_STR_INT_SIZE];
+        int size = read_line(fd, buf, PPK_STR_INT_SIZE);
+        buf[size - 1] = 0;
+        *field = atoi(buf);
+}
+
+
+static void ppk_parse_bitmap(struct ppk_content* content, int bitmap, char * options){
+        int options_value [4] = {0};
+
+        if (bitmap & PPK_FAST_PAT)
+                content->fast_pat = 1;
+        if (bitmap & PPK_NOCASE)
+                content->nocase = 1;
+
+        if ( !(bitmap & (PPK_DEPTH | PPK_OFFSET | PPK_DISTANCE | PPK_WITHIN)))
+                return;
+
+        char cur_option [PPK_STR_INT_SIZE] = {0};
+
+        int idx = 0;
+        int cur_option_int = 0;
+        for(int i = 0; i < strlen (options) + 1; i++){
+                if(options[i] == ' ' || options[i] == 0){
+                        cur_option_int = atoi(cur_option);   
+                        memset(cur_option, 0, PPK_STR_INT_SIZE);
+                        idx = 0;
+                        if (bitmap & PPK_DEPTH){
+                                content->depth = cur_option_int;
+                                bitmap &= ~(PPK_DEPTH & 0b1111);
+                        }
+                        else if (bitmap & PPK_OFFSET){
+                                content->offset = cur_option_int;
+                                bitmap &= ~(PPK_OFFSET & 0b1111);
+                        }
+
+                        else if (bitmap & PPK_DISTANCE){
+                                content->distance = cur_option_int;
+                                bitmap &= ~(PPK_DISTANCE & 0b1111);
+                        }
+
+                        else if (bitmap & PPK_WITHIN){
+                                content->within = cur_option_int;
+                                bitmap &= ~(PPK_WITHIN & 0b1111);
+                        }
+                        continue;
+                }
+                cur_option[idx++] = options[i];
+        }
+        printf ("content->fast_pat: %d\n", content->fast_pat);
+        printf ("content->nocase: %d\n", content->nocase);
+        printf ("content->depth: %d\n", content->depth);
+        printf ("content->offset: %d\n", content->offset);
+        printf ("content->distance: %d\n", content->distance);
+        printf ("content->within: %d\n", content->within);
+}
+
+static int ppk_read_contents(int fd, struct ppk_content* content){
+        content->pattern = malloc(sizeof(char) * content->size_pattern); 
+        if (!content->pattern)
+                PPK_ERR(ENOMEM,__func__);
+
+        content->pos = lseek (fd, 0, SEEK_CUR);
+        int ret = read(fd, content->pattern, content->size_pattern);
+        if(ret != content->size_pattern){
+                PPK_ERR (EIO,__func__);
+        }
+        lseek (fd, content->pos + content->size_pattern + 1, SEEK_SET);
+        
         return 0;
 }
 
-static void ppk_read_nrules(int fd, struct ppk_port_pair* port_pair){
-        char buf[10];
-        int size = read_line(fd, buf, PPK_LINE_SIZE);
-        buf[size - 1] = 0;
-        if(size < 0)
-                return;
-        port_pair->num_rules = atoi(buf);
+
+static void  ppk_read_nrules(int fd, struct ppk_port_pair* port_pair) {
+        ppk_read_int(fd, &port_pair->num_rules);
+
+        port_pair->rules = malloc(sizeof(struct ppk_rule) *
+                        port_pair->num_rules);
+
+        if (!port_pair->rules)
+                PPK_ERR(ENOMEM,__func__);
 }
 
-static void ppk_read_sid(int fd, struct ppk_rule* rule){
-        char buf[10];
-        int size = read_line(fd, buf, PPK_LINE_SIZE);
-        buf[size - 1] = 0;
-        if(size < 0)
-                return;
-        rule->sid = atoi(buf);
+static void ppk_read_options(int fd, struct ppk_content* content){
+        int bitmap;
+        ppk_read_int(fd, &bitmap);
+        char buf [PPK_LINE_SIZE] = {0};
+        read_line(fd, buf, PPK_LINE_SIZE);
+
+        ppk_parse_bitmap(content, bitmap, buf);
+        printf ("HAHHAHAHA\n");
+
 }
-
-static void ppk_read_num_contents(int fd, struct ppk_rule* rule){
-        char buf[10];
-        int size = read_line(fd, buf, PPK_LINE_SIZE);
-        buf[size - 1] = 0;
-        if(size < 0)
-                return;
-        rule->num_contents = atoi(buf);
-}
-
-static void ppk_parse_bitmap(struct ppk_rule* rule, int bitmap){
-        printf("bitmap = %d\n", bitmap);
-        if (bitmap & FAST_PAT){
-                printf("FP!!\n");
-                bitmap >> FAST_PAT;
-        }
-
-        if (bitmap & NOCASE){
-                printf("nocase!!\n");
-                bitmap >> NOCASE;
-        }
-
-        if (bitmap & DEPTH){
-                printf("depth!!\n");
-                bitmap >> DEPTH;
-        }
-
-        if (bitmap & OFFSET){
-                printf("offset!!\n");
-                bitmap >> OFFSET;
-        }
-
-        if (bitmap & DISTANCE){
-                printf("distance!!\n");
-                bitmap >> DISTANCE;
-        }
-
-        if (bitmap & WITHIN){
-                printf("within!!\n");
-                bitmap >> WITHIN;
-        }
-}
-
-static void ppk_read_bitmap(int fd, struct ppk_rule* rule){
-        char buf[10];
-        int buf_size = read_line(fd, buf, PPK_LINE_SIZE);
-        buf[buf_size - 1] = 0;
-        if(buf_size < 0)
-                return;
-        printf("AAAAAAAAAAAAAAAAAA\n");
-        ppk_parse_bitmap(rule, atoi(buf));
-}
-
-static void ppk_read_contents(int fd, struct ppk_rule* rule){
-        char buf[10];
-        int buf_size = read_line(fd, buf, PPK_LINE_SIZE);
-        buf[buf_size - 1] = 0;
-        if(buf_size < 0)
-                return;
-        
-        rule->contents = malloc(sizeof(struct ppk_contents*)*rule->num_contents);
-
-        int index_content = 0;
-        for(; index_content < rule->num_contents; index_content++){
-
-                rule->contents[index_content] = malloc(
-                                sizeof(struct ppk_content));
-                printf("AAAAAAAAAAAAAAAAAA\n");
-                rule->contents[index_content]->size_pattern = atoi(buf);
-
-                rule->contents[index_content]->pattern = malloc(atoi(buf));
-                buf_size = read_line(fd, rule->contents[index_content]->pattern,
-                               rule->contents[index_content]->size_pattern);
-
-                ppk_read_bitmap(fd, rule);
-        }
-}
-
-static void ppk_read_rule(int fd, struct ppk_rule* rule){
-        rule = malloc(sizeof(struct ppk_rule));
-        int curr_state = PPK_STATE_RSID;
-        while(1){
-                switch(curr_state){
-                        case PPK_STATE_RSID:
-                                ppk_read_sid(fd, rule);
-                                printf("sid = %d\n", rule->sid);
-                                curr_state = PPK_STATE_RNCONTENTS;
-                                break;
-                        case PPK_STATE_RNCONTENTS:
-                                ppk_read_num_contents(fd, rule);
-                                printf("n_contents = %ld\n", rule->num_contents);
-                                curr_state = PPK_STATE_RCONTENTS;
-                                break;
-                        case PPK_STATE_RCONTENTS:
-                                ppk_read_contents(fd, rule);
-                                curr_state = PPK_STATE_RBITMAP;
-                                return;
-                                break;
-                        case PPK_STATE_RBITMAP:
-                                ppk_read_bitmap(fd, rule);
-                                return;
-                                break;
-                }               
-        }
+static void ppk_read_ncontents (int fd, struct ppk_rule *rule,  int *num_contents)
+{
+        ppk_read_int(fd, num_contents);
+        rule->contents = malloc(sizeof(struct ppk_content) * rule->num_contents);
+        if (!rule->contents)
+                PPK_ERR (ENOMEM, __func__);
+        memset (rule->contents, 0, sizeof (struct ppk_content));
 }
 
 int ppk_automaton(int fd){
         int curr_state = PPK_STATE_RPORTS;
-        struct ppk_port_pair* port_pair;
-        struct ppk_rule* rule;
+        struct ppk_port_pair* curr_port_pair;
+        struct ppk_rule* curr_rule;
+        struct ppk_content *curr_content;
+        int content_index = 0;
+        int rule_index = 0;
+        int cont = 0;
         while(1){
                 switch (curr_state){
                         case PPK_STATE_RPORTS:
-                                port_pair = ppk_read_ports(fd);
+                                printf("%s\n","PPK_STATE_RPORTS");
+                                printf("cont = %d\n", cont++);
+                                curr_port_pair = ppk_read_ports(fd);
+                                if(!curr_port_pair)  
+                                       return 0; 
                                 curr_state = PPK_STATE_RNRULES;
                                 break;
                         case PPK_STATE_RNRULES:
-                                ppk_read_nrules(fd, port_pair);
+                                printf("%s\n","PPK_STATE_RNRULES");
+                                rule_index = 0;
+                                ppk_read_nrules(fd, curr_port_pair);
+                                curr_rule = &curr_port_pair->rules[rule_index];
                                 curr_state = PPK_STATE_RSID;
-                                // fill rule
-                                ppk_read_rule(fd, rule);
-                                return 0;
+                                break;
+                        case PPK_STATE_RSID:
+                                printf("%s\n","PPK_STATE_RSID");
+                                ppk_read_int(fd, &curr_rule->sid);
+                                curr_state = PPK_STATE_RNCONTENTS;
+                                break;
+                        case PPK_STATE_RNCONTENTS:
+                                printf("%s\n","PPK_STATE_RNCONTENTS");
+                                content_index = 0;
+                                ppk_read_ncontents (fd, curr_rule, &curr_rule->num_contents);
+                                curr_content = &curr_rule->contents[content_index];
+                                curr_state = PPK_STATE_RNBYTES;
+                                break;
+                        case PPK_STATE_RNBYTES:
+                                printf("%s\n","PPK_STATE_RNBYTES");
+                                ppk_read_int(fd, &curr_content->size_pattern);
+                                curr_state = PPK_STATE_RCONTENT;
+                                break;
+                        case PPK_STATE_RCONTENT:
+                                printf("%s\n","PPK_STATE_RCONTENT");
+                                ppk_read_contents(fd, curr_content);
+                                curr_state = PPK_STATE_ROPTIONS;
+                                break;
+                        case PPK_STATE_ROPTIONS:
+                                printf("%s\n","PPK_STATE_ROPTIONS");
+                                ppk_read_options(fd, curr_content);
+                                content_index++;
+                                if(content_index == curr_rule->num_contents){
+                                        printf ("AAA\n");
+                                        rule_index++;
+                                        if(rule_index == curr_port_pair->num_rules){
+                                                printf ("BBB\n");
+                                                curr_state = PPK_STATE_RPORTS;                
+                                        }
+                                        else{
+                                                printf ("CCC\n");
+                                                curr_rule = &curr_port_pair->rules[rule_index];
+                                                curr_state = PPK_STATE_RSID;
+                                        }
+                                }
+                                // nao eh ultimo content
+                                else{
+                                                printf ("DDD\n");
+                                        curr_content = &curr_rule->contents[content_index];
+                                        curr_state = PPK_STATE_RNBYTES;
+                                }
                                 break;
                 }
         }
-
 }
 
 
 int main(){
-        int fd = open("sapo_boi_tcp_rules.perereca", O_RDONLY);
+        int fd = open("sapo_boi_udp_rules.perereca", O_RDONLY);
         if (fd < 0) exit(-1);
 
         ppk_automaton(fd);
-        int x = 1;
 
-        //printf("%d\n", bitmap << x);
         return 0;
 }
 
--- ppk_parser.h
+++ ppk_parser.h
@@ -3,27 +3,35 @@
 
 #include<stdint.h>
 #include<stdbool.h>
+#include <errno.h>
 
-#define FAST_PAT  1 << 5
-#define NOCASE  1 << 4
-#define DEPTH  1 << 3
-#define OFFSET  1 << 2
-#define DISTANCE  1 << 1
-#define WITHIN  1
-#define BUF_SIZE 4096
+#define PPK_FAST_PAT  1 << 5
+#define PPK_NOCASE  1 << 4
+#define PPK_DEPTH  1 << 3
+#define PPK_OFFSET  1 << 2
+#define PPK_DISTANCE  1 << 1
+#define PPK_WITHIN  1
+#define PPK_BUF_SIZE 4096
 
 #define PPK_STATE_RPORTS 0
 #define PPK_STATE_RNRULES 1
-#define PPK_STATE_RSID 2
-#define PPK_STATE_RNCONTENTS 3
-#define PPK_STATE_RCONTENTS 4
-#define PPK_STATE_RBITMAP 5
+#define PPK_STATE_RRULES 2 
+#define PPK_STATE_RSID 3
+#define PPK_STATE_RNCONTENTS 4
+#define PPK_STATE_RCONTENTS 5
+#define PPK_STATE_RNBYTES 6
+#define PPK_STATE_RCONTENT 7
+#define PPK_STATE_ROPTIONS 8
 
 #define PPK_LINE_SIZE 1024
+#define PPK_STR_INT_SIZE 10
+
+//#define PPK_ERR (a) do{printf ("ERROR: %s: %s", stra (a), __func__, ##__VA_ARGS__),exit (a)}while(0)
+//define PPK_ERR(a, ...) printf("%s(): " a, __func__, ##__VA_ARGS__)
 
 struct ppk_content{
         char* pattern;
-        ssize_t size_pattern;
+        int size_pattern;
 
         bool fast_pat;
         bool nocase;
@@ -31,23 +39,24 @@ struct ppk_content{
         int offset;
         int distance;
         int within;
+        off_t pos;
 };
 
 struct ppk_rule{
         uint32_t sid;
-        ssize_t num_contents;
-        struct ppk_content** contents;  // so por enquanto, depois colocar ponteiro p/ automato
+        int num_contents;
+        struct ppk_content* contents;  // so por enquanto, depois colocar ponteiro p/ automato
 };
 
 struct ppk_port_pair{
 	int* src_port;
-	ssize_t size_src_port;
+	int size_src_port;
 
 	int* dst_port;
-	ssize_t size_dst_port;
+	int size_dst_port;
 
         int num_rules;
-        struct ppk_rule** rules;
+        struct ppk_rule* rules;
 };
 
 
