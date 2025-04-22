3a8852c6c1786bbba8e1ca51d92752560a763127
Raphael Kaviak Machnicki <raphael@email.com>
2025-04-22 18:02:07 -0300

    Finalizado

--- ids/af_xdp_kern.c
+++ ids/af_xdp_kern.c
@@ -15,9 +15,19 @@
                          ##__VA_ARGS__);                \
 })
 
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, __u32);
+    __type(value, __u64);
+} time_map SEC(".maps");
+
 SEC("xdp")
 int xdp_ids_func(struct xdp_md *ctx)
 {
+
+    __u64 time = bpf_ktime_get_ns();
 
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
@@ -41,13 +51,16 @@ int xdp_ids_func(struct xdp_md *ctx)
     } else if (eth_type == bpf_htons(ETH_P_IPV6)) {
         ip_type = parse_ip6hdr(&nh, data_end, &ip6h);
     }
+    else
+        return XDP_PASS;
 
     void* end_ip = nh.pos; // aponta pro final do cabeçalho IP
     char* letter;
     char method[5];
+    int delta;
     if (ip_type == IPPROTO_TCP) {
         if (parse_tcphdr(&nh, data_end, &tcph) > 0) {
-            bpf_trace_printk("num deu 1", 10);
+            //bpf_trace_printk("num deu 1", 10);
                 if (nh.pos + sizeof(tcph) > data_end){
                         return -1;
                 }
@@ -61,21 +74,44 @@ int xdp_ids_func(struct xdp_md *ctx)
                         method[3] = *(letter + 2);
                         method[4] = '\0';
                         bpf_printk("%s\n", method);
+                        char* new_content = (char*)end_ip;
+                        if (new_content + sizeof(method) > data_end)
+                                return -1;
+                        #pragma unroll
+                        for(int i = 0; i < sizeof(method); i++){
+                                *(new_content + i) = *(method + i);
+                        }
+                        delta = data_end - (end_ip + 5);
+                        bpf_printk("delta1 = %d\n", delta);
+                        if(bpf_xdp_adjust_tail(ctx, 0-delta) < 0){
+                                bpf_printk("Deu pau 1\n");
+                                return -1;
+                        }
+                        time = bpf_ktime_get_ns() - time;
+                        bpf_printk("time1 = %lld\n", time);
+                }
+                else{
+                        // esse é o caso que não é HTTP
+                        delta = data_end - end_ip;
+                        if(bpf_xdp_adjust_tail(ctx, 0-delta) < 0){
+                                bpf_printk("Deu pau 2.. delta = %d\n", delta);
+                                return -1;
+                        }
+                        bpf_printk("delta2 = %d\n", delta);
+                        time = bpf_ktime_get_ns() - time;
+                        bpf_printk("time2 = %lld\n", time);
                 }
         }
     }
-    int delta = data_end - (end_ip + 5);
-    bpf_xdp_adjust_tail(ctx, delta);
-    //bpf_trace_printk("aaaaaaa", 8);
-    *end_ip = method[0];
-    *(end_ip + 1) = method[1];
-    *(end_ip + 2) = method[2];
-    *(end_ip + 3) = method[3];
-    *(end_ip + 4) = method[4];
-    bpf_printk("%s\n", *end_ip);
 
-
-
+    __u32 k = 0;
+    __u64 *v;
+    v = bpf_map_lookup_elem(&time_map, &k);
+    if (v) {
+        __sync_fetch_and_add(v, time);
+        bpf_map_update_elem(&time_map, &k, v, BPF_ANY);
+        bpf_printk("time = %d\n", *v);
+    }
     return action;
 }
 
--- send_packet.py
+++ send_packet.py
@@ -1,7 +1,15 @@
 from scapy.all import TCP, IP, Raw, sendp, Ether, IFACES, UDP, wrpcap
+import random
 
-data = 'GETasfdsadf'
-a = Ether() / IP(src='10.11.1.2', dst='10.11.1.1') / TCP(sport=23, dport=32) / Raw(load=data)
+methods = ['GET', 'POST', 'HEAD']
+lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis ipsum sit amet tempus vestibulum. Nunc aliquam lectus non aliquam molestie. Quisque nec porttitor massa, rutrum aliquam velit. Nam in convallis ante. Suspendisse potenti. Praesent blandit consectetur quam, ultrices congue urna lobortis ut. Proin semper lacus imperdiet orci euismod, vel dapibus felis porttitor. Curabitur porttitor vestibulum lacinia. Sed ac molestie augue, vel varius augue. Etiam eu enim orci.Fusce ut felis varius, auctor diam vel, dictum purus. Morbi elementum mauris leo, sed vestibulum lacus ultricies at. Etiam augue lacus, volutpat sed aliquet eu, tincidunt sed lorem. Donec in elementum purus. Donec tempus non turpis a lacinia. Donec suscipit justo nec enim tempus dictum. Sed laoreet sagittis purus vel sollicitudin. Integer tempor et arcu sed aliquet.Vestibulum tempus aliquet nisi, in vehicula dui rhoncus et. Suspendisse fringilla consequat dolor sit amet ultricies. Suspendisse pulvinar turpis ante, sit amet ullamcorper ex molestie eu. Donec a lorem at augue porta hendrerit at porta eros. In tincidunt ultricies tortor ac vehicula. In purus ex, viverra sit amet mauris ac, ultricies ornare velit. Vestibulum sagittis tempor felis. Suspendisse malesuada nulla venenatis, blandit dolor eu, elementum dolor. Mauris id aliquet lectus, eu auctor turpis. Praesent facilisis tempor neque, ac leo."
+for i in range(1000*1000):
+    data = methods[random.randint(0, 2)] + lorem
+    a = Ether() / IP(src='10.11.1.2', dst='10.11.1.1') / TCP(sport=23, dport=32) / Raw(load=data)
+    wrpcap("validationBPF2.pcap", a, append=True)
+
+'''
 iface = IFACES.dev_from_name("veth0")
 sendp(a, iface=iface)
+'''
 
