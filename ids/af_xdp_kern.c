dd82d24fff156b153f7fdd2100abb7eb6cab59b7
Raphael Kaviak Machnicki <raphael@email.com>
2026-01-15 18:14:39 -0300

    Add code to pass and handle packets to/in userspace

--- ids/af_xdp_kern.c
+++ ids/af_xdp_kern.c
@@ -2,10 +2,6 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h> /* bpf_core_type_id_local */
 #include "xdp/parsing_helpers.h"
-#include "common_kern_user.h"
-
-#define MAX_MTU 1520  // MTU 1500 bytes
-                      //
 
 #undef bpf_printk
 #define bpf_printk(fmt, ...)                            \
@@ -22,6 +18,13 @@ struct {
     __type(key, __u32);
     __type(value, __u64);
 } time_map SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_XSKMAP);
+	__uint(max_entries, 1);
+	__uint(key_size, sizeof(int));
+	__uint(value_size, sizeof(int));
+} xsks_map SEC(".maps");
 
 SEC("xdp")
 int xdp_ids_func(struct xdp_md *ctx)
@@ -52,48 +55,56 @@ int xdp_ids_func(struct xdp_md *ctx)
         ip_type = parse_ip6hdr(&nh, data_end, &ip6h);
     }
     else
-        return XDP_PASS;
+        // Se não for nem IP nem IPv6 (ARP, por exemplo), descarta
+        return XDP_DROP;
 
     void* end_ip = nh.pos; // aponta pro final do cabeçalho IP
     void *end_tcp = NULL;
     char* letter;
-    char method[5];
+    char method[3];
     int delta;
     if (ip_type == IPPROTO_TCP) {
         if (parse_tcphdr(&nh, data_end, &tcph) > 0) {
-            //bpf_trace_printk("num deu 1", 10);
                 if (nh.pos + sizeof(tcph) > data_end){
                         return -1;
                 }
                 end_tcp = nh.pos;
                 letter = nh.pos + 1;
-                bpf_printk("letra = %d\n", *letter);
-                bpf_printk("letra + 1 = %d\n", *(letter + 1));
+                //bpf_printk("letra = %d\n", *letter);
+                //bpf_printk("letra + 1 = %d\n", *(letter + 1));
                 if (*letter == 'E' || *letter == 'O'){
                         method[0] = *((char*)nh.pos);
                         method[1] = *letter;
-                        method[2] = *(letter + 1);
-                        method[3] = *(letter + 2);
-                        method[4] = '\0';
-                        bpf_printk("%s\n", method);
+                        //method[2] = *(letter + 1);
+                        //method[3] = *(letter + 2);
+                        method[2] = '\0';
+                        bpf_printk("Metodo do HTTP: %s\n", method);
                         char* new_content = (char*)end_tcp;
                         if (new_content + sizeof(method) > data_end)
                                 return -1;
+
                         #pragma unroll
                         for(int i = 1; i < sizeof(method); i++){
                                 *(new_content + i) = *(method + i);
                         }
-                        delta = data_end - (end_tcp + 5);
-                        bpf_printk("delta1 = %d\n", delta);
+
+                        delta = data_end - (end_tcp + 3);
+
+                        //bpf_printk("delta1 = %d\n", delta);
+                        
                         if(bpf_xdp_adjust_tail(ctx, 0-delta) < 0){
                                 bpf_printk("Deu pau 1\n");
                                 return -1;
                         }
-                        time = bpf_ktime_get_ns() - time;
-                        bpf_printk("time1 = %lld\n", time);
+                        //time = bpf_ktime_get_ns() - time;
+                        //bpf_printk("time1 = %lld\n", time);
                 }
                 else{
+                        //  Pelo que eu entendi, é pra remover os pacotes que não são HTTP
+                        return XDP_DROP;
+
                         // esse é o caso que não é HTTP
+                        /*
                         delta = data_end - end_ip;
                         if(bpf_xdp_adjust_tail(ctx, 0-delta) < 0){
                                 bpf_printk("Deu pau 2.. delta = %d\n", delta);
@@ -102,10 +113,12 @@ int xdp_ids_func(struct xdp_md *ctx)
                         bpf_printk("delta2 = %d\n", delta);
                         time = bpf_ktime_get_ns() - time;
                         bpf_printk("time2 = %lld\n", time);
+                        */
                 }
         }
     }
 
+    /*
     __u32 k = 0;
     __u64 *v;
     v = bpf_map_lookup_elem(&time_map, &k);
@@ -114,7 +127,12 @@ int xdp_ids_func(struct xdp_md *ctx)
         bpf_map_update_elem(&time_map, &k, v, BPF_ANY);
         bpf_printk("time = %d\n", *v);
     }
-    return action;
+    */
+
+    //return action;
+    __u32 rx_queue_index = ctx->rx_queue_index;
+    bpf_printk("Recebido na fila %d\n", rx_queue_index);
+    return bpf_redirect_map(&xsks_map, rx_queue_index, 0);
 }
 
 char _license [] SEC ("license") = "GPL";
--- ids/loader.c
+++ ids/loader.c
@@ -12,41 +12,184 @@
 #include <xdp/libxdp.h>
 #include <sys/resource.h>
 
-struct xdp_program * prog;
+#include "xsk_socket.h"
+#include "common_defines.h"
+#include <poll.h>
 
-static void int_exit(int sig)
+struct xdp_program * prog;
+// Sobrescrever a seguinte variável com o índice da interface desejada
+int ifindex = 7;
+int n_queues = 1;
+int global_exit = 0;
+
+static void exit_application(int sig)
 {
+        xdp_program__detach(prog, ifindex, XDP_MODE_SKB, 0);
         xdp_program__close(prog);
-        exit(0);
+        // Sai do laço principal do programa
+        global_exit = 1;
+}
+
+static inline void handle_receive_packets(struct xsk_socket_info* xsk_info){
+        uint32_t idx_rx = 0;
+        uint32_t idx_fq = 0;
+        int ret;
+        unsigned int frames_received, stock_frames;
+
+        // ver se no RX tem alguma coisa
+        frames_received = xsk_ring_cons__peek(&xsk_info->rx, RX_BATCH_SIZE, &idx_rx);  // prenche a var idx_rx
+                                                                                       // se não recebeu nada, volta pro loop de pool
+        if(!frames_received)
+                return;
+
+        // se chegou aqui, recebi pelo menos um pacote nesse socket
+        stock_frames = xsk_prod_nb_free(&xsk_info->umem->fq, xsk_info->umem_frame_free);
+
+        if(stock_frames > 0){
+                // reserva stock_frames slots no ring fill da UMEM
+                ret = xsk_ring_prod__reserve(&xsk_info->umem->fq, stock_frames, &idx_fq);
+
+                // This should not happen, but just in case
+                while (ret != stock_frames)
+                        ret = xsk_ring_prod__reserve(&xsk_info->umem->fq, frames_received, &idx_fq);
+
+                for(int i = 0; i < stock_frames; i++){
+                        *xsk_ring_prod__fill_addr(&xsk_info->umem->fq, idx_fq++) = xsk_alloc_umem_frame(xsk_info);
+                }
+                xsk_ring_prod__submit(&xsk_info->umem->fq, stock_frames);
+        }
+
+        // só agora que vou tratar os pacotes recebidos (!!!!!!!!!)
+
+        uint64_t addr;
+        uint32_t len;
+
+        for(int i = 0; i < frames_received; i++){
+                // lê o descritor armazenado em idx_rx
+                addr = xsk_ring_cons__rx_desc(&xsk_info->rx, idx_rx)->addr;
+                len = xsk_ring_cons__rx_desc(&xsk_info->rx, idx_rx)->len;
+                idx_rx++;
+
+                //COLOCANDO O PACOTE NUM PCAP
+
+
+                // adiciona o endereço à lista de endereços disponíveis do fill ring da UMEM
+                xsk_free_umem_frame(xsk_info, addr);
+        }
+
+        // libera os frames recebidos do RX (indica pro kernel que eu já li essas posições)
+        xsk_ring_cons__release(&xsk_info->rx, frames_received);
 }
 
 int main ()
 {
         struct rlimit rlim = {RLIM_INFINITY, RLIM_INFINITY};
+        // Definindo que não há limites de memória para a execução deste programa
         if (setrlimit(RLIMIT_MEMLOCK, &rlim)) {
-                puts("asnd");
                 return -1;
         }
-        prog = xdp_program__open_file ("af_xdp_kern.o", "xdp", 0);
+
+        struct sigaction action;
+        action.sa_handler = exit_application;
+        sigemptyset(&action.sa_mask);
+        action.sa_flags = 0;
+        sigaction(SIGINT, &action, NULL);
+
+        struct config cfg = {
+                .do_unload = true,
+                .filename = "af_xdp_kern.o",
+                .progsec = "xdp",
+                .batch_pkts = BATCH_PKTS_DEFAULT,
+        };
+
+        cfg.xsk_bind_flags = XDP_COPY;
+        cfg.ifname = "microsec";
+        cfg.ifindex = 7;
+
+        prog = xdp_program__open_file("af_xdp_kern.o", "xdp", 0);
+
         if (!prog)
         {
                 printf ("xdp_program__open_file error");
                 return 1;
         }
 
-        int ifindex = 7;
 
-        int ret = xdp_program__attach (prog, ifindex, XDP_MODE_SKB, 0);
+        int ret = xdp_program__attach(prog, ifindex, XDP_MODE_SKB, 0);
         if (ret) {
                 printf ("xdp_program__attach error");
                 return ret;
         }
 
-        struct bpf_object *bpf_obj = xdp_program__bpf_obj (prog);
-        //signal(SIGINT, int_exit);
-        //signal(SIGTERM, int_exit);
-        
-        printf ("done %d", getpid());
-        return 0;
 
+        struct bpf_object *bpf_obj = xdp_program__bpf_obj(prog);
+
+        struct bpf_map *map;
+        map = bpf_object__find_map_by_name(bpf_obj, "xsks_map");
+
+        int xsks_map_fd;
+        xsks_map_fd = bpf_map__fd(map);
+        if (xsks_map_fd < 0) {
+                fprintf(stderr, "ERROR: no xsks map found: %s\n",
+                                strerror(xsks_map_fd));
+                exit(EXIT_FAILURE);
+        }
+
+        //Configure and initialize AF_XDP sockets  (vetor de ponteiros!!)
+
+        struct xsk_umem_info **umems;
+        struct xsk_socket_info **xsk_sockets;
+
+        umems = (struct xsk_umem_info **)
+                malloc(sizeof(struct xsk_umem_info *) * n_queues);
+        xsk_sockets = (struct xsk_socket_info **)
+                malloc(sizeof(struct xsk_socket_info *) * n_queues);
+
+        if(!umems || !xsk_sockets){
+                printf("Não consegui alocar o vetor de UMEMS ou o vetor de sockets!\n");
+        }
+
+        // this function configures UMEMs and XSKs
+        if(!af_xdp_init(umems, xsk_sockets, n_queues, &cfg)){
+                printf("Tudo certo!!\n");
+        }
+
+        // fill xsks map
+        enter_xsks_into_map(xsks_map_fd, xsk_sockets, n_queues);
+
+
+        // Primeiro, faz polling pra receber na UMEM
+        struct pollfd fds[n_queues];  // Essa estrutura é entendida pela syscall poll(), que é usada para verificar se há novos eventos no socket
+        for(int i = 0; i < n_queues; i++)
+                memset(fds, 0, sizeof(fds));
+        int i_queue;
+
+        for(i_queue = 0; i_queue < n_queues; i_queue++){
+                fds[i_queue].fd = xsk_socket__fd(xsk_sockets[i_queue]->xsk);
+                fds[i_queue].events = POLLIN;  // POLLIN = "there is data to read"
+        }
+
+        while(!global_exit){
+                ret = poll(fds, n_queues, -1);
+                if(ret <= 0)
+                        continue;  // nenhum evento
+                for(i_queue = 0; i_queue < n_queues; i_queue++){
+                        if(fds[i_queue].revents & POLLIN){
+                                printf("recebi na fila %d\n", i_queue);
+                                // Nessa função, salvo pacotes redirecionados num pcap
+                                handle_receive_packets(xsk_sockets[i_queue]);
+                        }
+                }
+        }
+
+        // Cleanup
+        for (int i_queue = 0; i_queue < n_queues; i_queue++) {
+                xsk_socket__delete(xsk_sockets[i_queue]->xsk);
+                xsk_umem__delete(umems[i_queue]->umem);
+        }
+        free(umems);
+        free(xsk_sockets);
+
+        return 0;
 }
+
